d0c98c06efdb265dd58039fba36dd82a8e7d411c
darren90 <1005052145@qq.com>
2016-03-20 22:16:53 +0800

    pod update

--- GitHuber/GitHuber/TFCommon.swift
+++ GitHuber/GitHuber/TFCommon.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 
+public let KRandomColor = UIColor.init(colorLiteralRed: Float(arc4random_uniform(256))/255.0, green: Float(arc4random_uniform(256))/255.0, blue: Float(arc4random_uniform(256))/255.0, alpha: 1.0)
+
+
+public func KColor(r:Int,g:Int,b:Int) -> UIColor{
+    return UIColor.init(colorLiteralRed: Float(r)/255.0, green: Float(g)/255.0, blue: Float(b)/255.0, alpha: 1.0)
+}
+
+public let KBasePlayUrl = "http://127.0.0.1:12345"
+
+
+//友盟
 public let KAppid = "56ee9ee467e58e3b2500035e"
+
+
+public let KHeight = UIScreen.mainScreen().bounds.height
+public let KWidth = UIScreen.mainScreen().bounds.width
 
 
 
