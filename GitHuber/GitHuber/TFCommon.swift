1d4e4adabb9bb3160131c4875197bec963a51f01
darren90 <1005052145@qq.com>
2016-03-20 21:04:35 +0800

    init githuber

---
+++ GitHuber/GitHuber/TFCommon.swift
@@ -0,0 +1,15 @@
+//
+//  TFCommon.swift
+//  GitHuber
+//
+//  Created by Tengfei on 16/3/20.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+import UIKit
+
+
+public let KAppid = "56ee9ee467e58e3b2500035e"
+
+
+
