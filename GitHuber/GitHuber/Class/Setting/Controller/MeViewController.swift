1b5aa5ae1a1aa19af48a85837014022c27f8a066
darren90 <1005052145@qq.com>
2016-03-22 22:56:23 +0800

    init home

---
+++ GitHuber/GitHuber/Class/Setting/Controller/MeViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MeViewController.swift
+//  GitHuber
+//
+//  Created by Tengfei on 16/3/22.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MeViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("")
+        return cell!;
+    }
+    
+    
+}
+
