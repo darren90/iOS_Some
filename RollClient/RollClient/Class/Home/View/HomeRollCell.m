6d53ad5e8a6f948fa920d1e553170fcaa02f6dac
Fengtf <1005052145@qq.com>
2016-04-01 14:10:59 +0800

    等分布局

--- RollClient/RollClient/Class/Home/View/HomeRollCell.m
+++ RollClient/RollClient/Class/Home/View/HomeRollCell.m
@@ -16,6 +16,11 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageView2;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView3;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *buttonW;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imag1X;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imag2X;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imag3X;
+
 @end
 
 @implementation HomeRollCell
@@ -27,6 +32,29 @@
         cell = [[NSBundle mainBundle] loadNibNamed:@"HomeRollCell" owner:nil options:nil].firstObject;
     }
     return cell;
+}
+
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+    
+    
+}
+
+
+-(void)updateConstraints
+{
+    [self autoArrangeBoxWithConstransits:@[self.imag1X,self.imag2X,self.imag3X] width:self.buttonW.constant];
+    [super updateConstraints];
+}
+
+-(void)autoArrangeBoxWithConstransits:(NSArray *)constraninArray width:(CGFloat)width{
+    NSInteger step = ([UIScreen mainScreen].bounds.size.width - (width * constraninArray.count))/(constraninArray.count+1);
+    for (int i = 0 ; i< constraninArray.count ; i++) {
+        NSLayoutConstraint *constraint = constraninArray[i];
+        constraint.constant = step * (i + 1) + width * i;
+    }
 }
 
 
@@ -55,10 +83,6 @@
     
 }
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
