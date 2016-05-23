857f9d39a8613c17406378d7a0514501106d5ede
darren90 <1005052145@qq.com>
2016-05-23 23:02:04 +0800

    add

---
+++ TFMp3Player/TFMp3Player/Classes/Controller/Mp3PlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  Mp3PlayingViewController.h
+//  TFMp3Player
+//
+//  Created by Tengfei on 16/5/23.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Mp3PlayingViewController : UIViewController
+
+@end
---
+++ TFMp3Player/TFMp3Player/Classes/Controller/Mp3PlayingViewController.m
@@ -0,0 +1,37 @@
+//
+//  Mp3PlayingViewController.m
+//  TFMp3Player
+//
+//  Created by Tengfei on 16/5/23.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import "Mp3PlayingViewController.h"
+
+@interface Mp3PlayingViewController ()
+
+@end
+
+@implementation Mp3PlayingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
