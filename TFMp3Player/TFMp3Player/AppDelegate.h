0e68034f7cb50eb5f97b7f85e596a6c54d8219df
darren90 <1005052145@qq.com>
2016-05-23 22:23:21 +0800

    add

---
+++ TFMp3Player/TFMp3Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TFMp3Player
+//
+//  Created by Tengfei on 16/5/23.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TFMp3Player/TFMp3Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TFMp3Player
+//
+//  Created by Tengfei on 16/5/23.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
