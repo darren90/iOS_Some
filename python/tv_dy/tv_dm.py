22a4f15cd6fab7fd51ad84470cf81ef0fce121c7
darren90 <1005052145@qq.com>
2016-05-29 18:22:43 +0800

    add

--- python/tv_dy/tv_dm.py
+++ python/tv_dy/tv_dm.py
@@ -64,7 +64,7 @@ class spider(object):
         return pages_arary
 # 获取每个页面待爬取的item具体内容的链接
     def get_all_urls(self,all_pages):
-       links_arary = set()
+       links_arary = [] #set()
        for page_url in all_pages:
            if page_url is None:
              break
@@ -81,6 +81,9 @@ class spider(object):
              if link.get_text() is not None:
                 # print type(link)
                 # print len(link.contents)
+
+                # 装一下数据的字典
+                link_dic = {}
 
                 # 时间，日期
                 t_time = ""
@@ -111,56 +114,43 @@ class spider(object):
                         # 类别对应的链接地址
                         new_url = tag_a['href']
                         t_url = urlparse.urljoin(page_url,new_url)
-                print 't_time:%s' % t_time
-                print 't_info:%s' % t_info
-                print 't_title:%s' % t_title
-                print 't_url:%s' % t_url
-                print '-----------------end---'
-                    # full_url = urlparse.urljoin(url,new_url)
-                    # full_url = full_url + 'index.html'
-                    # # print '%s - %s ' % (title , full_url)
-                    # all_dic = {}
-                    # all_dic["url"]= full_url
-                    # all_dic["category"] = title
-                    # all_categorys.append(all_dic)
-
-
-           # if links is  None:
-           #    break
-           # for link in links.find_all('li'):
-           #    new_link = link.find('a')['href']   
-           #    # print new_link
-           #    links_arary.add(new_link)
+                link_dic["time"] = t_title
+                link_dic["info"] = t_info
+                link_dic["title"] = t_title
+                link_dic["url"] = t_url
+                links_arary.append(link_dic)
        return links_arary
 
 #解析具体的数据
-    def parse_html_data(self,html_countent):
+    def parse_html_data(self,link_dic):
+     t_time = link_dic["time"]
+     t_info = link_dic["info"] 
+     t_title = link_dic["title"]
+     t_url = link_dic["url"] # 带爬去下载地址的文件
+
+     html = mySpider.getsource(t_url)
+     if html is None:
+        return
+
      res_array = []
-     res_data = {}
-     soup = BeautifulSoup(html_countent, "html.parser",from_encoding='utf-8') 
-     title = soup.find('div',class_="infotitle").find('h1')
-     icon_url = soup.find('div',class_="bpic l").find('img')['src']
-     # print title.get_text()
-     print '-- %s' % title.get_text()
-     down_types = soup.find('ul',class_="yuanItem").find_all('li')
-     # print down_types
-     count = 1
-     for down_type in down_types:
-        down_type_str = down_type.get_text().strip('\n')
-        print '-- start :-%s -' % (down_type_str)
-        keydoword = 'tabxinfan_1_tab_%d' % count        
-        down_urls = soup.find(id=keydoword).find_all('li')# down_urls = soup.find(id="tabxinfan_1_tab_2").find_all('li')
-        for downurl in down_urls:
-            # print title.get_text()
-            t_title = downurl.get_text().strip('\n')
-            u_url = downurl.find('a')['href'].strip('\n')
-            sql = "insert into DBURL (tv_name,tv_iconUrl,tv_downType,tv_downSeries,tv_downSeries_url) values (\"%s\",'%s','%s','%s','%s');" % (title.get_text().encode('utf-8'),icon_url,down_type_str, t_title, u_url)
-            # sql = "insert into DBURL (tv_name,tv_iconUrl,tv_downType,tv_downSeries,tv_downSeries_url) values ('%s','%s','%s','%s','%s') " % title.get_text() 
-            res_data["sql"] = sql
-            res_array.append(res_data)
-            print sql
-        count = count +1
-        print '-- end---------'
+
+     soup = BeautifulSoup(html, "html.parser",from_encoding='utf-8') 
+
+     content = soup.find('div',class_="co_content8")
+     if content is None:
+        return
+     links = content.find_all('table')
+     for link in links:
+        res_data = {}
+        url = link.get_text().replace(" ","").replace("\"","\'").replace("\t","").replace('\n','').replace(' ','').strip().lstrip().rstrip(',')   
+        # print 'url:%s' % url 
+        # ftp://bbs.forlu.com_free:forlu_2ra6r@ftp.forlu.com/new/03.12/" target="_blank
+        url = url.replace("\" target=\"_blank","").replace("\<a%20href=\\","")
+        sql = "insert into DYURL (tv_name,tv_iconUrl,tv_state,tv_viewcount,tv_type,tv_dub,tv_showtime,tv_updatetime,tv_info,tv_downType,tv_downSeries,tv_downSeries_url) \
+             values (\"%s\",'%s',\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\");" % (t_title,"","",t_time,"","","",t_time,t_info,"", "", url)
+        res_data["sql"] = sql
+        # print sql
+        res_array.append(res_data)
      return res_array
  
 if __name__ == '__main__':
@@ -170,24 +160,23 @@ if __name__ == '__main__':
     classinfo = []
     url = 'http://www.dy2018.com/html/dongman/' 
     mySpider = spider()
-    all_pages = mySpider.get_all_pages(url,1) #所有的页面 
+    all_pages = mySpider.get_all_pages(url,50) #所有的页面 
     all_links = mySpider.get_all_urls(all_pages)
-    # for link in all_links:
-    #     # print link
-    #     try:
-    #         html = mySpider.getsource(link)
-    #         res_array = mySpider.parse_html_data(html)
-    #         for res_data in res_array:
-    #             fout.write(res_data["sql"].encode('utf-8') + '\n')
-    #         count = count +1
-    #     except Exception as e:
-    #         if fialCount == 10200:
-    #             fout.close()
-    #             break
-    #         fialCount = fialCount + 1
-    #         count = count +1
-    #         fout.write("-- Craw fail %d " % count + '\n')
-    #         print '-- Craw fail %d ,excepton:%s' % (count,e)
+    for link_dic in all_links:
+        try:
+            res_array = mySpider.parse_html_data(link_dic)
+            for res_data in res_array:
+                fout.write(res_data["sql"].encode('utf-8') + '\n')
+                print 'success:%d' % (count)
+            count = count +1
+        except Exception as e:
+            print '-- Craw fail %d ,excepton:%s' % (count,e)
+            if fialCount == 10200:
+                fout.close()
+                break
+            fialCount = fialCount + 1
+            count = count +1
+            fout.write("-- Craw fail %d " % count + '\n')
     fout.close()
  
 
