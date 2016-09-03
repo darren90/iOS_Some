a1580b693159d1001ec610353e87abdec04d1c8b
darren90 <1005052145@qq.com>
2016-09-03 12:34:59 +0800

    add

--- FileMaster/FileMaster/Class/Setting/Controller/Setting_RootController.m
+++ FileMaster/FileMaster/Class/Setting/Controller/Setting_RootController.m
@@ -13,6 +13,7 @@
 #import "SettingGroup.h"
 #import "UseViewController.h"
 #import "ShareActivity.h"
+#import "UIView+Toast.h"
 
 //扫一扫
  
@@ -164,13 +165,13 @@
 -(void)shareActionTapped
 {
     //要分享的内容，加在一个数组里边，初始化UIActivityViewController
-    NSString *textToShare = @"我是且行且珍惜_iOS，欢迎关注我！";
+    NSString *textToShare = @"万能的播放器我非常喜欢，推荐你也是使用";
     UIImage *imageToShare = [UIImage imageNamed:@"movie_icon"];
-    NSURL *urlToShare = [NSURL URLWithString:@"https://github.com/wslcmk"];
+    NSURL *urlToShare = [NSURL URLWithString:@"http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=%@&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8"];
     NSArray *activityItems = @[urlToShare,textToShare,imageToShare];
     
     //自定义Activity
-    ShareActivity * custom = [[ShareActivity alloc] initWithTitie:@"且行且珍惜_iOS" withActivityImage:[UIImage imageNamed:@"wang"] withUrl:urlToShare withType:@"customActivity" withShareContext:activityItems];
+    ShareActivity * custom = [[ShareActivity alloc] initWithTitie:@"飞哥" withActivityImage:[UIImage imageNamed:@"movie_icon"] withUrl:urlToShare withType:@"customActivity" withShareContext:activityItems];
     NSArray *activities = @[custom];
     
     /**
@@ -183,22 +184,15 @@
      */
     UIActivityViewController *activityVC = [[UIActivityViewController alloc]initWithActivityItems:activityItems applicationActivities:activities];
     
-    //UIActivityViewControllerCompletionWithItemsHandler)(NSString * __nullable activityType, BOOL completed, NSArray * __nullable returnedItems, NSError * __nullable activityError)  iOS >=8.0
-    
-    //UIActivityViewControllerCompletionHandler (NSString * __nullable activityType, BOOL completed); iOS 6.0~8.0
-    
-    
     if([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0){
         //初始化回调方法
         UIActivityViewControllerCompletionWithItemsHandler myBlock = ^(NSString *activityType,BOOL completed,NSArray *returnedItems,NSError *activityError)
         {
             NSLog(@"activityType :%@", activityType);
-            if (completed)
-            {
+            if (completed)  {
                 NSLog(@"completed");
             }
-            else
-            {
+            else  {
                 NSLog(@"cancel");
             }
             
@@ -207,7 +201,6 @@
         // 初始化completionHandler，当post结束之后（无论是done还是cancell）该blog都会被调用
         activityVC.completionWithItemsHandler = myBlock;
     }else{
-        
         UIActivityViewControllerCompletionHandler myBlock = ^(NSString *activityType,BOOL completed)
         {
             NSLog(@"activityType :%@", activityType);
@@ -224,24 +217,6 @@
         // 初始化completionHandler，当post结束之后（无论是done还是cancell）该blog都会被调用
         activityVC.completionHandler = myBlock;
     }
-    
-    //Activity 类型又分为“操作”和“分享”两大类
-    /*
-     UIKIT_EXTERN NSString *const UIActivityTypePostToFacebook     NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypePostToTwitter      NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypePostToWeibo        NS_AVAILABLE_IOS(6_0);    //SinaWeibo
-     UIKIT_EXTERN NSString *const UIActivityTypeMessage            NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypeMail               NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypePrint              NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypeCopyToPasteboard   NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypeAssignToContact    NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypeSaveToCameraRoll   NS_AVAILABLE_IOS(6_0);
-     UIKIT_EXTERN NSString *const UIActivityTypeAddToReadingList   NS_AVAILABLE_IOS(7_0);
-     UIKIT_EXTERN NSString *const UIActivityTypePostToFlickr       NS_AVAILABLE_IOS(7_0);
-     UIKIT_EXTERN NSString *const UIActivityTypePostToVimeo        NS_AVAILABLE_IOS(7_0);
-     UIKIT_EXTERN NSString *const UIActivityTypePostToTencentWeibo NS_AVAILABLE_IOS(7_0);
-     UIKIT_EXTERN NSString *const UIActivityTypeAirDrop            NS_AVAILABLE_IOS(7_0);
-     */
     
     // 分享功能(Facebook, Twitter, 新浪微博, 腾讯微博...)需要你在手机上设置中心绑定了登录账户, 才能正常显示。
     //关闭系统的一些activity类型
