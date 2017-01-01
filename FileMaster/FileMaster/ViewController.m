1e1f95e1f8c96c1bce81e2eeb5168bbb1a4836b2
darren90 <1005052145@qq.com>
2017-01-01 23:03:24 +0800

    添加Apple watch图片

--- FileMaster/FileMaster/ViewController.m
+++ FileMaster/FileMaster/ViewController.m
@@ -17,6 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    // 字典 --> 模型对象
+//    NSDictionary *dict = @{@"name" : @"china",@"age":@18,@"height":@2.0};
+//    Person *p = [[Person alloc]init];
+//    [p setValuesForKeysWithDictionary:dict];
+//    
+//     //  模型对象 --> 字典
+//    NSDictionary *dict2 = [self dictionaryWithValuesForKeys:@"name",@"age",@"height"];
 }
 
 - (void)didReceiveMemoryWarning {
